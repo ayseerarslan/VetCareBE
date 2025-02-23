48cc90655ed4c42244664367df55ea321aa70e82
ayseerarslan <ayseerarslan94@gmail.com>
2025-02-23 14:30:00 +0000

    Start

---
+++ src/main/java/erarslan/ayse/VetCare/VetCareApplication.java
@@ -0,0 +1,13 @@
+package erarslan.ayse.VetCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erarslan/ayse/VetCare/VetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package erarslan.ayse.VetCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

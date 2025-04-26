64914586f90deb06c0fe7355884f52c88d811007
ayseerarslan <ayseerarslan94@gmail.com>
2025-04-26 21:03:46 +0100

    webconfig is edited

--- src/main/java/erarslan/ayse/VetCare/config/WebConfig.java
+++ src/main/java/erarslan/ayse/VetCare/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+//                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("https://vet-care-ui.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*")
